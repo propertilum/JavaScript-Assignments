6c5efee6e9c44b43a484edf0ac4c7cff7751bd57
propertilum <mkamran3787@gmail.com>
2026-05-14 20:38:45 +0500

    DONE

--- Chapter 26-30/app.js
+++ Chapter 26-30/app.js
@@ -1,62 +1,62 @@
 // CHAPTER #26-30
 // ANSWER #1
 
-    // var userinput1 = +prompt("Enter your number")
+    var userinput1 = +prompt("Enter your number")
 
-    // document.writeln(
-    //     "number: " + userinput1 + "<br>" +
-    //     "round off value: " + Math.round(userinput1) + "<br>" +
-    //     "floor value: " + Math.floor(userinput1) + "<br>" +
-    //     "ceil value: " + Math.ceil(userinput1)
-    // )
+    document.writeln(
+        "number: " + userinput1 + "<br>" +
+        "round off value: " + Math.round(userinput1) + "<br>" +
+        "floor value: " + Math.floor(userinput1) + "<br>" +
+        "ceil value: " + Math.ceil(userinput1)
+    )
 
 // ANSWER #2
 
-    // var userinput2 = parseFloat(prompt("Enter your number"))
+    var userinput2 = parseFloat(prompt("Enter your number"))
 
-    // document.writeln(
-    //     "number: " + userinput2 + "<br>" +
-    //     "round off value: " + Math.round(userinput2) + "<br>" +
-    //     "floor value: " + Math.floor(userinput2) + "<br>" +
-    //     "ceil value: " + Math.ceil(userinput2)
-    // )
+    document.writeln(
+        "number: " + userinput2 + "<br>" +
+        "round off value: " + Math.round(userinput2) + "<br>" +
+        "floor value: " + Math.floor(userinput2) + "<br>" +
+        "ceil value: " + Math.ceil(userinput2)
+    )
 
 // ANSWER #3
 
-    // var userinput3 = +prompt("Enter your number")
+    var userinput3 = +prompt("Enter your number")
 
-    // document.writeln(
-    //     "The absolute value of " + userinput3 + " is " + Math.abs(userinput3)
-    // )
+    document.writeln(
+        "The absolute value of " + userinput3 + " is " + Math.abs(userinput3)
+    )
 
 // ANSWER #4
 
-    // var randomNum = Math.ceil(Math.random() * 6)
+    var randomNum = Math.ceil(Math.random() * 6)
 
-    // document.write(
-    //     "random dice value: " + randomNum
-    // )
+    document.write(
+        "random dice value: " + randomNum
+    )
 
 // ANSWER #5
 
-    // var randomNum2 = Math.ceil(Math.random() * 2)
+    var randomNum2 = Math.ceil(Math.random() * 2)
 
 
-    // if(randomNum2 == 1){
-    //     document.writeln("random coin value: Heads")
-    // }
-    // else{
-    //     document.writeln("random coin value: Tails")
-    // }
+    if(randomNum2 == 1){
+        document.writeln("random coin value: Heads")
+    }
+    else{
+        document.writeln("random coin value: Tails")
+    }
 
 // ANSWER #6
 
-    // var randomNum3 = Math.ceil(Math.random() * 100)
+    var randomNum3 = Math.ceil(Math.random() * 100)
 
 
-    // document.writeln(
-    //     "random number between 1 and 100: " + randomNum3
-    // )
+    document.writeln(
+        "random number between 1 and 100: " + randomNum3
+    )
 
 // ANSWER #7
 // ANSWER #8
